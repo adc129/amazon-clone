85c62f7605ba9854ee8375d2f1123296eab52455
adc129 <60369328+adc129@users.noreply.github.com>
2020-12-07 17:27:39 -0600

    ad

--- amazon/src/components/Checkout.js
+++ amazon/src/components/Checkout.js
@@ -12,7 +12,7 @@ function Checkout() {
       <div className="checkout__left">
         <img
           className="checkout__ad"
-          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/sns/email/SnS-Creative-Desktop._CB1579804990_.png"
           alt=""
         />
 
